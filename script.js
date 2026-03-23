43a15e62f79720f0ce54e75779e485936e9bd8bf
Owen <stefanescuowen@gmail.com>
2026-03-23 11:06:39 -0400

    Worked on hamburger menu.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function toggleMenu() {
     const nav = document.getElementById("nav-links");
     const burger = document.querySelector(".hamburger");
-
     nav.classList.toggle("active");
     burger.classList.toggle("active");
 }
+
+// Submenu toggles
+document.querySelectorAll('#nav-links > li > a[href="#"]').forEach(link => {
+    link.classList.add("toggle-submenu"); // add arrow
+
+    link.addEventListener("click", (e) => {
+        e.preventDefault(); // prevent # jump
+        const submenu = link.nextElementSibling;
+
+        if (submenu) {
+            submenu.classList.toggle("active");
+            link.classList.toggle("active");
+        }
+    });
+});
